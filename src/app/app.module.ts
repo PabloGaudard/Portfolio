a5f0dea4e61b823d6989a53a419841c6ab41f488
Pablo Gaudard <pablogaudard@gmail.com>
2022-11-22 10:17:32 -0300

    Init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './components/container/container.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
